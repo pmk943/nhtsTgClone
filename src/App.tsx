b4460c8d0565572266aaa64bf5132180a0859852
pmk943 <pmk943@gmail.com>
2021-12-09 15:00:25 +0530

    Admin Home page Just started

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { Route, Routes } from "react-router";
+import AdminHome from "./components/AdminHome";
 import Login from "./components/Login";
+import NoRoute from "./components/Login/NoRoute";
+import SignIn from "./components/Login/SignIn";
 
 function App() {
   return (
     
     <Routes>
-      <Route path="/" element={<Login />}></Route>
+      <Route path="/" element={<Login />} >
+            <Route index element={<SignIn />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="*" element ={<NoRoute />} />  
+      </Route>
+      <Route path="/admin" element={<AdminHome />} />
+
 
     </Routes>
   );
