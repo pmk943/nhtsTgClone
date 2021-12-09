d445a6a28d93a5131a2d638dff6c8909d678e228
pmk943 <pmk943@gmail.com>
2021-12-09 14:58:11 +0530

    Added No Route Component for the Not mathing URLs and also changed structure for Routing using nested Routing

---
+++ src/components/Login/NoRoute.tsx
@@ -0,0 +1,9 @@
+function NoRoute() {
+    return (
+        <div>
+            <h1>Sorry, We didn't make any components for this, its just for the show case :)</h1>
+        </div>
+    )
+}
+
+export default NoRoute
