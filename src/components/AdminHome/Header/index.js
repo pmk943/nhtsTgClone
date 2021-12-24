9cb7e20a653a2df04f5bf484d76a8fdf576acd61
pmk943 <pmk943@gmail.com>
2021-12-24 12:42:36 +0530

    Added headers

--- src/components/AdminHome/Header/index.js
+++ src/components/AdminHome/Header/index.js
@@ -1,17 +1,23 @@
-import './Header.css'
+import "./Header.css";
 function Header() {
-    return (
-        <div className="Header__conatiner">
-            <i class="bi bi-list" />
-            <span>Nutrition and health Tracking System</span>
-            <img src="http://nhts.telangana.gov.in/img/logo_small.png" alt="TG emblem" />
-            <button>
-                <span>PEBBAIR</span>
-                <i class="bi bi-caret-down" />
-            </button>
-            
-        </div>
-    )
+  return (
+    <div className="Header__conatiner">
+      <div>
+        <i class="bi bi-list" />
+        <span>Nutrition and health Tracking System</span>
+      </div>
+      <div className="Header__conatiner__right">
+        <img
+          src="http://nhts.telangana.gov.in/img/logo_small.png"
+          alt="TG emblem"
+        />
+        <button>
+          <span>PEBBAIR</span>
+          <i class="bi bi-caret-down" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
