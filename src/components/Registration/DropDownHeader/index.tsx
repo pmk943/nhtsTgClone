91635833f3836cd1f18f3fdae8c883076121b9ed
laxmi kishore <laxmikishore1@gmail.com>
2021-12-28 12:18:51 +0530

    RegistrationForm-Parent

--- src/components/Registration/DropDownHeader/index.tsx
+++ src/components/Registration/DropDownHeader/index.tsx
@@ -1,43 +1,52 @@
-import { useState } from "react"
-import { REGISTRATION_DROPDOWN_OPTIONS  } from "../../../utils/constants"
-const projectName:string = REGISTRATION_DROPDOWN_OPTIONS.projectName
+import { useState } from "react";
+import { REGISTRATION_DROPDOWN_OPTIONS } from "../../../utils/constants";
+const projectName: string = REGISTRATION_DROPDOWN_OPTIONS.projectName;
 function DropDownHeader() {
-    const [sector, setSector]= useState(["select sector"])
-    const [awcs, setAwc]= useState([])
-    const [selectedAwc, setSelecetdAwc]= useState("select awc")
+  const [sector, setSector] = useState(["select sector"]);
+  const [awcs, setAwc] = useState([]);
+  const [selectedAwc, setSelecetdAwc] = useState("select awc");
 
-    const onSectorChange = (e : any) =>{
-        const selectedSector:string = e.target.value;
-        setAwc(REGISTRATION_DROPDOWN_OPTIONS.awcs[selectedSector])
-        setSelecetdAwc("select awc")
-    }
-    const onAwcChange = (e:any)=>{
-        setSelecetdAwc(e.target.value)
-    }
-    return (
-        <form >
-            <label htmlFor="projectName">
-                Project Name
-                <select id="projectName">
-                    <option>{projectName}</option>
-                </select>
-            </label>
-            <label >
-                Select Sector
-                <select onChange={e=>onSectorChange(e)}>
-                    
-                    {REGISTRATION_DROPDOWN_OPTIONS.sectors.map((sector:string,idx:number) =>(idx===0 ?<option key={sector} disabled selected >{sector}</option>:<option key={sector}>{sector}</option>))}
-                </select>
-            </label>
-            <label >
-               Select AWC 
-               <select  onChange={onAwcChange} value={selectedAwc}>
-               {awcs.map((sector) =><option key={sector}>{sector}</option>)}
-
-               </select>
-            </label>
-        </form>
-    )
+  const onSectorChange = (e: any) => {
+    const selectedSector: string = e.target.value;
+    setAwc(REGISTRATION_DROPDOWN_OPTIONS.awcs[selectedSector]);
+    setSelecetdAwc("select awc");
+  };
+  const onAwcChange = (e: any) => {
+    setSelecetdAwc(e.target.value);
+  };
+  return (
+    <form>
+      <label htmlFor="projectName">
+        Project Name
+        <select id="projectName">
+          <option>{projectName}</option>
+        </select>
+      </label>
+      <label>
+        Select Sector
+        <select onChange={(e: any) => onSectorChange(e)}>
+          {REGISTRATION_DROPDOWN_OPTIONS.sectors.map(
+            (sector: string, idx: number) =>
+              idx === 0 ? (
+                <option key={sector} disabled selected>
+                  {sector}
+                </option>
+              ) : (
+                <option key={sector}>{sector}</option>
+              )
+          )}
+        </select>
+      </label>
+      <label>
+        Select AWC
+        <select onChange={onAwcChange} value={selectedAwc}>
+          {awcs.map((sector: any) => (
+            <option key={sector}>{sector}</option>
+          ))}
+        </select>
+      </label>
+    </form>
+  );
 }
 
-export default DropDownHeader
+export default DropDownHeader;
--- src/customhooks/useInput.tsx
+++ src/customhooks/useInput.tsx
@@ -1,35 +1,52 @@
-import React, { ChangeEventHandler, useState } from "react"
+import React, { ChangeEventHandler, useState } from "react";
 
-function useInput(sendingFormData:Function,regValidation:Function) {
-    
-    type valuesObjType = {
-        name?: string;
-        husband_name?: string;
-        household_No?:number;
-        serial_no_family?:number;
-        aadhar_id?:number;
-    };
+function useInput(sendingFormData: Function, regValidation: Function) {
+  type valuesObjType = {
+    name?: string;
+    husband_name?: string;
+    household_No?: number;
+    serial_no_family?: number;
+    aadhar_id?: number;
+    mobile_number?: number;
+    calendar_entry?: string;
+    age_text_entry?: number;
+    caste_category?: string;
+    Registration_date?: string;
+    LMP?: string;
+    permanent_temparory?: string;
+    height?: number;
+    weight?: number;
+  };
 
-    const[values, setValue] = useState<valuesObjType>({})
-    const[errorDetails, setErrorDetails] = useState<valuesObjType>({})
+  const [values, setValue] = useState<valuesObjType>({});
+  const [errorDetails, setErrorDetails] = useState<valuesObjType>({});
 
-    const onFormSubmit =(event:React.FormEvent) =>{
-        event.preventDefault();
-        const errors=regValidation(values)
-        setErrorDetails(errors)
-        if(Object.keys(errors).length === 0){
-            console.log("inside form data")
-            sendingFormData()
-        }
-        
+  const onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const errors = regValidation(values);
+    setErrorDetails(errors);
+    if (Object.keys(errors).length === 0) {
+      console.log("inside form data");
+      sendingFormData();
     }
+  };
 
-    const onInputChange = (event:React.ChangeEvent<HTMLInputElement>) =>{
-     setValue(values =>({...values,[event.target.name]:event.target.value}))
-    }
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue((values) => ({
+      ...values,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
-    
-    return {errorDetails , values, onInputChange,onFormSubmit}
+  const onResetSubmit = () => {
+    setValue((values) => ({
+      ...values,
+      household_No: 0,
+    }));
+    console.log(values);
+  };
+
+  return { errorDetails, values, onInputChange, onFormSubmit, onResetSubmit };
 }
 
-export default useInput
+export default useInput;
